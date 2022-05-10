a080c6d2cf5996f35760536bf34d20991e62207f
Natthanon Manop <natthanon.manop@gmail.com>
2022-05-11 03:46:39 +0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const configs = require("./configs");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
