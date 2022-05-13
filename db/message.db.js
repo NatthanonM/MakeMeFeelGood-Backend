cefc77d88017f55c32ce9b8055e4bcb6a4117d8d
karnkittik <karnkitti_tang@hotmail.com>
2022-05-13 19:52:56 +0700

    edit time attribute name and sorter

--- db/message.db.js
+++ db/message.db.js
@@ -32,7 +32,7 @@ const createMessage = async (message, voice_id) => {
     text: message,
     upvote: 0,
     voice_id: voice_id,
-    time: utcTimestamp
+    created_at: utcTimestamp,
   };
   return res;
 };
@@ -48,17 +48,22 @@ const getMessages = async (start) => {
         "#T": "text",
         "#UV": "upvote",
         "#VI": "voice_id",
-        "#CA": "created_at"
+        "#CA": "created_at",
       },
       ExpressionAttributeValues: {
         ":start": start,
         ":stop": stop,
       },
       FilterExpression: "created_at >= :start AND created_at < :stop",
-      ProjectionExpression: "#ID, #T, #UV, #VI, #CA", 
+      ProjectionExpression: "#ID, #T, #UV, #VI, #CA",
     };
     var data = await docClient.scan(params).promise();
-    var sortedItems = data.Items.sort((a, b) => b.upvote - a.upvote);
+    var sortedItems = data.Items.sort((a, b) => {
+      if (a.upvote === b.upvote) {
+        return b.created_at - a.created_at;
+      }
+      return b.upvote - a.upvote;
+    });
   } catch (err) {
     console.log("[DB]: ", err, err.stack);
   }
