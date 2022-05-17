6b6489b90d6e4f45c376d059c8663d2bbb3dcc50
Natthanon Manop <natthanon.manop@gmail.com>
2022-05-17 17:01:58 +0700

    Revert "add cors options, pm2"
    This reverts commit 0965dcf947041158bb20ee36613cbf4f9b88a048.

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const compression = require("compression");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const configs = require("./configs");
 
-var corsOptions = {
-  origin: configs.origin,
-};
-
-app.use(cors(corsOptions));
-app.use(compression());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- configs/index.js
+++ configs/index.js
@@ -3,7 +3,6 @@ const configs = {
   region: process.env.REGION || "",
   S3BucketName: process.env.S3_BUCKET_NAME || "",
   dynamodbTableName: process.env.DYNAMODB_TABLE_NAME || "",
-  origin: process.env.ORIGIN || "",
 };
 
 module.exports = configs;
